2ab143d8dfb5e03e451bf47564be5347ab273c21
Michal Wrzosek <michal@wrzosek.pl>
2020-11-13 14:31:39 +0100

    Updates

---
+++ precache-manifest.fb7ec141dfa29889a981e9cf0efa4e77.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94dd7004aeb0ee6f62e3c3fb2f0bdc3b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4fc0764834bb7c297067",
+    "url": "/static/css/main.795991f2.chunk.css"
+  },
+  {
+    "revision": "2cae72b420e0f1cd7d1a",
+    "url": "/static/js/2.20d4c540.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.20d4c540.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fc0764834bb7c297067",
+    "url": "/static/js/main.6fe2126f.chunk.js"
+  },
+  {
+    "revision": "bc51d2a1e6b2c6d65a5b",
+    "url": "/static/js/runtime-main.1bd49777.js"
+  }
+]);
